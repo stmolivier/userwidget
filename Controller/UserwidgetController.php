95005fed18e5b5a03420257ceb16689a763d8a7f
user <user@g1101615virtuel.chu-amiens.local>
2015-05-29 10:23:35 +0200

    initial tree

---
+++ Controller/UserwidgetController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Simusante\UserwidgetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * userwidget controller.
+ *
+ */
+class UserwidgetController extends Controller
+{
+
+}
---
+++ SimusanteUserwidgetBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Simusante\UserwidgetBundle;
+
+use Claroline\CoreBundle\Library\PluginBundle;
+use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
+
+class SimusanteUserwidgetBundle extends PluginBundle
+{
+    public function getConfiguration($environment)
+    {
+        $config = new ConfigurationBuilder();
+
+        return $config->addRoutingResource(__DIR__ . '/Resources/config/routing.yml', null, 'simuser');
+    }
+}
