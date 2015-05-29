2c7755ad43f24c1737124443a0eb5e6cd69c4467
user <user@g1101615virtuel.chu-amiens.local>
2015-05-29 14:12:08 +0200

    retrieve user from workspace

--- Controller/UserwidgetController.php
+++ Controller/UserwidgetController.php
@@ -4,7 +4,8 @@ namespace Simusante\UserwidgetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
-use Symfony\Component\HttpFoundation\Response;
+use Claroline\CoreBundle\Persistence\ObjectManager;
+use JMS\DiExtraBundle\Annotation as DI;
 
 /**
  * userwidget controller.
@@ -12,8 +13,44 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class UserwidgetController extends Controller
 {
+    private $om;
+    private $userRepo;
+    private $roleRepo;  //nécessaire pour récupérer le role sous le bon format
+    private $wsRepo;
+    /**
+     * @DI\InjectParams({
+     *      "om"                 = @DI\Inject("claroline.persistence.object_manager")
+     * })
+     */
+    public function __construct(
+        ObjectManager $om
+    )
+    {
+        //Object manager initialization
+        $this->om                 = $om;
+        //user repo access
+        $this->userRepo           = $om->getRepository('ClarolineCoreBundle:User');
+        //role repo access
+        $this->roleRepo           = $om->getRepository('ClarolineCoreBundle:Role');
+        //ws repo access
+        $this->wsRepo            = $om->getRepository('ClarolineCoreBundle:Workspace\Workspace');//$om->getRepository(Entity)
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function displayUserAction()
     {
-        return $this->render('SimusanteUserwidgetBundle::toto.html.twig');
+        /*
+        //Récupère un objet Role
+        $role = $this->roleRepo->findOneByName('ROLE_WS_CREATOR');
+        //Récupère la liste des utilisateur ayant le role
+        $users = $this->userRepo->findByRoles(array($role));
+        */
+        $workspace = $this->wsRepo->findOneByName('ws2');
+        //Récupère un objet Role
+        $users = $this->userRepo->findUsersByWorkspace(array($workspace));
+
+        return $this->render('SimusanteUserwidgetBundle::toto.html.twig', array('users'=> $users));
     }
 }
